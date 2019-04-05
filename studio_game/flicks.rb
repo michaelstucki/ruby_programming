e3341e909df6766a8203ed7ea48df56ecd5dcaf8
Michael Stucki <orange.urn@gmail.com>
2019-04-05 14:39:32 -0600

    Add Variables and Objects exercise solutions

---
+++ studio_game/flicks.rb
@@ -0,0 +1,14 @@
+puts 'Mikey loves Goonies'
+puts 'Mikey\'s favorite movie is Goonies'
+puts "Mikey's favorite movie is Goonies"
+
+movie = 'Goonies'
+puts 'Mikey\'s favorite movie is ' + movie
+
+rank = 10
+
+puts movie + ' has a rank of ' + rank.to_s
+
+puts "Mikey's favorite movie is #{movie}."
+
+puts "#{movie} has a rank of #{rank * 2}."
--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,5 +1,11 @@
-greeting = "Welcome!"
-3.times do
-	puts greeting.upcase
-end
-puts Time.new
+name1 = "larry"
+health1 = 60
+puts "#{name1}'s health is #{health1 / 9.0}"
+puts "#{name1}'s health is #{health1 / 9}"
+
+puts "Players: \n\tlarry \n\tcurly \n\tmoe"
+
+name2 = "curly"
+name3 = "moe"
+
+puts "Players: \n\t#{name1} \n\t#{name2} \n\t#{name3}"
