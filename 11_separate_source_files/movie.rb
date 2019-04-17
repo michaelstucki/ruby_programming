3bcbe0e6f3879490d0658963d7a7da6870d42ab7
Michael Stucki <orange.urn@gmail.com>
2019-04-17 15:00:45 -0600

    Add Separate Source Files exercise solutions

---
+++ 11_separate_source_files/movie.rb
@@ -0,0 +1,39 @@
+class Movie
+  attr_reader :rank     # read only
+  attr_accessor :title  # read-write
+
+  def initialize(title, rank=0)
+    @title = title.capitalize
+    @rank = rank
+  end
+
+  def normalized_rank # a virtual attribute, derives from instance variables
+    @rank / 10
+  end
+
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+
+  def to_s
+    "#{@title} has a rank of #{@rank}"
+  end
+end
+
+if __FILE__ == $0 # only run this if the currently running file is this file
+  movie = Movie.new("goonies", 10)
+  puts movie.title
+  puts movie.rank
+
+  movie.thumbs_up
+  movie.thumbs_up
+
+  movie.thumbs_down
+  puts movie.rank
+  puts movie
+end
+
