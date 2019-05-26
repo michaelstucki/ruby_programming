cb121e52f79141462a6f6f4f2de4f95527cf676a
Michael Stucki <orange.urn@gmail.com>
2019-05-26 07:10:15 -0600

    Add Modules

--- 14_modules/conditionals.rb
+++ 14_modules/conditionals.rb
@@ -1,6 +1,6 @@
 require_relative 'movie'
 
-movie = Movie.new("godfather", 10 )
+movie = Movie.new("godfather", 10)
 
 if movie.rank >= 10
 	puts "Hit"
---
+++ 14_modules/playlist.rb
@@ -0,0 +1,23 @@
+require_relative 'movie'
+require_relative 'waldorf_and_statler'
+
+class Playlist
+  def initialize(name)
+    @name = name
+    @movies = []
+  end
+
+  def add_movie(movie)
+    @movies << movie
+  end
+
+  def play
+    puts "#{@name}'s playlist:"
+    puts @movies
+
+    @movies.each do |movie|
+      WaldorfAndStatler.review(movie)
+      puts movie
+    end
+  end
+end
